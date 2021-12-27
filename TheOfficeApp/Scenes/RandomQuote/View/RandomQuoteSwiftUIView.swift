6d74fb953bb8eeef7c3642780b5ba4d54440ce79
Daniel Teodoro <daniel.missimat@gmail.com>
2021-12-26 23:12:37 -0300

    Adds Spacing enum and loading screen for SwiftUI

--- TheOfficeApp/Scenes/RandomQuote/View/RandomQuoteSwiftUIView.swift
+++ TheOfficeApp/Scenes/RandomQuote/View/RandomQuoteSwiftUIView.swift
@@ -12,33 +12,26 @@ struct RandomQuoteSwiftUIView: View {
     
     var body: some View {
         ZStack {
-            VStack (alignment: .center, spacing: 16) {
-                Spacer().frame(height: 16)
+            VStack (alignment: .center, spacing: SpacingValues.base01) {
+                Spacer().frame(height: SpacingValues.base01)
                 Text("Random Quote").font(.title)
                 Spacer().frame(height: 32)
                 Text(viewModel.content ?? "").onAppear(perform: fetch)
                     .font(.system(size: 24, weight: .regular, design: .default))
                     .multilineTextAlignment(.leading)
-                    .padding(16)
+                    .padding(SpacingValues.base01)
                 HStack(alignment: .center) {
                     Text(viewModel.characterFullName ?? "").onAppear(perform: fetch)
-                        .font(.system(size: 16, weight: .medium, design: .default))
-                        .padding(16)
+                        .font(.system(size: SpacingValues.base01, weight: .medium, design: .default))
+                        .padding(SpacingValues.base01)
                     Spacer()
                 }
                 Spacer()
                 Button("Get a new quote", action: fetch)
-                    .padding(16)
+                    .padding(SpacingValues.base01)
             }.onAppear(perform: fetch)
             if viewModel.isLoading {
-                ZStack {
-                    Color(.gray)
-                        .opacity(0.5)
-                        .ignoresSafeArea()
-                    ProgressView()
-                        .progressViewStyle(CircularProgressViewStyle(tint: .gray))
-                        .scaleEffect(2)
-                }
+                LoadingSwiftUIView()
             }
         }
     }
